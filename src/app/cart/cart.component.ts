6d55a547649e3b70179b0310435fb4eacc203f50
Rajugupta1996 <rajugupta1996@gmail.com>
2020-09-09 10:50:01 +0530

    competed checkout feature

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -7,11 +8,21 @@ import { CartService } from '../cart.service';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-   items;
-  constructor(private cartService: CartService) { }
+   items; checkoutForm;
+  constructor(private cartService: CartService,private formBuilder: FormBuilder,) { this.checkoutForm = this.formBuilder.group({
+      name: '',
+      address: ''
+    });}
 
   ngOnInit() {
     this.items = this.cartService.getItems();
+  }
+   onSubmit(customerData) {
+    // Process checkout data here
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
+
+    console.warn('Your order has been submitted', customerData);
   }
 
 }
